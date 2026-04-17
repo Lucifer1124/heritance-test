8ae89d26e718b87c3929dfeee0b8adb5b152920f
Lucifer1124 <puspalbiswas11gd11@gmail.com>
2026-04-17 15:06:26 +0530

    Testing with Jenkins

---
+++ test.java
@@ -0,0 +1,28 @@
+class Tree {
+    String type;
+    String color;
+    Tree(String type, String color) {
+        this.type = type;
+        this.color = color;
+    }
+}
+class Branch extends Tree {
+    Branch(String type, String color) {
+        super(type, color);
+    }
+}
+class Leaf extends Branch {
+    Leaf(String type, String color) {
+        super(type, color);
+    }
+    void printDetails() {
+        System.out.println("Type: " + type);
+        System.out.println("Color: " + color);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Leaf myLeaf = new Leaf("Cherry Blossom", "Pink");
+        myLeaf.printDetails();
+    }
+}
