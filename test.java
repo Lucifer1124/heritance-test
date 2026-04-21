acdd7a02413d1b67c624a9b8103de9bfd096fba4
Lucifer1124 <puspalbiswas11gd11@gmail.com>
2026-04-21 14:08:50 +0530

    Changing

--- test.java
+++ test.java
@@ -1,28 +1,32 @@
 class Tree {
     String type;
     String color;
+	String location;
     Tree(String type, String color) {
         this.type = type;
         this.color = color;
+		this.location = location;
     }
 }
 class Branch extends Tree {
     Branch(String type, String color) {
-        super(type, color);
+        super(type, color, location);
     }
 }
 class Leaf extends Branch {
     Leaf(String type, String color) {
-        super(type, color);
+        super(type, color, location);
     }
+}
     void dis() {
         System.out.println("Type: " + type);
         System.out.println("Color: " + color);
+		System.out.println("Location: " + location);
     }
 }
 public class test {
     public static void main(String[] args) {
-        Leaf obj = new Leaf("Cherry Blossom", "Pink");
+        Leaf obj = new Leaf("Cherry Blossom", "Pink", "Japan");
         obj.dis();
     }
 }
